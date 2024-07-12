84bba57d58af47824cfa2146ab3c6b487cea0aff
Jeongmo Seo <wjdah1402@gmail.com>
2024-07-12 12:57:39 +0900

    :sparkles: feat: 앱 아이콘, 색상, 사진 추가

---
+++ frontend/lib/const/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0xFF0C4DE6);
+const SECONDARY_COLOR = Color(0xFFF8F6D8);
+const THIRD_COLOR = Color(0xFF73D3F8);
+
+final TEXT_FIELD_FILL_COLOR = Colors.grey[300];
